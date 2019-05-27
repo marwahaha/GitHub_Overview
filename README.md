f83fdd217e4a1c2b2fe9ae12e254243f6c737b9d
Joske de Jong <joskedejong@gmail.com>
2019-05-27 15:26:48 +0200

    [f] added function to swicht tags from static back to fixed

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Goal: to make a presentation page for my GitHub projects. The end-result should 
 * * abondoned 'sticky' keyword as it dit nog provide the desired functionality
 * [x] make tab stick to folder once folder is scrolled into view
 * * [ ] make alignment for sticking smooth
+* * [ ] make tabs detach when scrolling back up
 * [ ] add descriptions
 * [ ] add explanation of [s], [f] and [b]
 
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function() {
     var folderHeight = [];
+    var h = document.documentElement.clientHeight;
 
     function screenHeight(){                                            // sets article blocks to screen height
-        var h = document.documentElement.clientHeight;
         let l = document.getElementsByClassName("folder");
         for(var i = 0; i < l.length; i++){
             l[i].style.height = h+"px";
@@ -18,14 +18,24 @@ $(document).ready(function() {
     }
     console.log(folderHeight[1]);
     console.log(folderHeight[2]);
+    console.log(folderHeight[3]);
+    console.log(folderHeight[4]);
+    console.log(folderHeight[5]);
+    console.log(folderHeight[6]);
+    console.log(folderHeight[7]);
+    console.log(folderHeight[8]);
 
     window.onscroll = function(){                                       // function to switch 'tabs' from fixed to static 
         var scrolled = document.body.scrollTop;
         console.log(scrolled);
         for(x=1; x<9; x++){
-            if (folderHeight[x] < scrolled){ 
+            if ((folderHeight[x] - h) < scrolled){ 
                 // $(".tab"+x).css("display", "static");
                 document.getElementsByClassName("tab"+x)[0].style.position = "static";
+            }
+            else if ((folderHeight[x] - h) > scrolled){ 
+                // $(".tab"+x).css("display", "static");
+                document.getElementsByClassName("tab"+x)[0].style.position = "fixed";
             }
         }
     }
